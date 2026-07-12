358bbde7ec284a986b5da91e5656fb8dadee0340
usjuandiegot-droid <us.juandiegot@gmail.com>
2026-07-12 11:03:58 -0500

    Update procesador.py

--- procesador.py
+++ procesador.py
@@ -1,6 +1,9 @@
+import shutil
+
+
 def procesar_colombia(archivo_entrada, archivo_salida):
-    pass
+    shutil.copy(archivo_entrada, archivo_salida)
 
 
 def procesar_peru(archivo_entrada, archivo_salida):
-    pass
+    shutil.copy(archivo_entrada, archivo_salida)
